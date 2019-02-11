f7c8de00b2b5d084febc9f39852acf1169002c47
Christopher Woods <chryswoods@gmail.com>
2019-02-11 16:30:50 +0000

    Creating a basic "reset"

--- source/admin/reset.py
+++ source/admin/reset.py
@@ -14,6 +14,4 @@ def run(args):
     status = 0
     message = "Resetting service..."
 
-    return_value = create_return_value(status, message)
-
-    return return_value
+    return {"status": status, "message": message}
