2af95ea06169e5f31c34eb7b9420aa3d4be5d766
Christopher Woods <chryswoods@gmail.com>
2019-02-24 17:42:40 +0000

    Refactoring complete - all unit tests pass locally

--- test/pytest/Service/test_service_object.py
+++ test/pytest/Service/test_service_object.py
@@ -1,43 +1,57 @@
 
 from Acquire.Identity import IdentityService
-from Acquire.Service import Service
+from Acquire.Service import Service, push_is_running_service, \
+       pop_is_running_service, push_testing_objstore, \
+       pop_testing_objstore
 from Acquire.Crypto import PrivateKey
 
 
-def test_service_object(aaai_services):
-    service = IdentityService(Service(service_type="identity",
-                                      service_url="identity"))
+def test_service_object(tmpdir_factory):
+    bucket = tmpdir_factory.mktemp("test_service")
+    push_testing_objstore(bucket)
+    push_is_running_service()
 
-    assert(service.uid() is not None)
-    assert(service.is_identity_service())
-    assert(not service.should_refresh_keys())
-    assert(service.is_unlocked())
-    assert(not service.is_locked())
+    try:
+       service = IdentityService(Service(service_type="identity",
+                                          service_url="identity"))
 
-    passphrase = PrivateKey.random_passphrase()
+       assert(service.uid() is not None)
+       assert(service.is_identity_service())
+       assert(not service.should_refresh_keys())
+       assert(service.is_unlocked())
+       assert(not service.is_locked())
 
-    data = service.to_data(passphrase)
+       passphrase = PrivateKey.random_passphrase()
 
-    service2 = IdentityService.from_data(data, passphrase)
+       data = service.to_data(passphrase)
 
-    assert(service2.uid() == service.uid())
-    assert(service2.is_unlocked())
-    assert(not service2.is_locked())
-    assert(service2.is_identity_service())
-    assert(service.canonical_url() == service2.canonical_url())
-    assert(not service2.should_refresh_keys())
+       service2 = IdentityService.from_data(data, passphrase)
 
-    keys = service.dump_keys()
+       assert(service2.uid() == service.uid())
+       assert(service2.is_unlocked())
+       assert(not service2.is_locked())
+       assert(service2.is_identity_service())
+       assert(service.canonical_url() == service2.canonical_url())
+       assert(not service2.should_refresh_keys())
 
-    keys = service.load_keys(keys)
+       keys = service.dump_keys()
 
-    assert(keys[service.private_key().fingerprint()] == service.private_key())
-    assert(keys[service.private_certificate().fingerprint()] ==
-           service.private_certificate())
+       keys = service.load_keys(keys)
 
-    service.refresh_keys()
+       assert(keys[service.private_key().fingerprint()] == service.private_key())
+       assert(keys[service.private_certificate().fingerprint()] ==
+              service.private_certificate())
 
-    assert(service.last_key_update() > service2.last_key_update())
-    assert(service.last_certificate().public_key() ==
-           service2.public_certificate())
-    assert(service.last_key() == service2.private_key())
+       service.refresh_keys()
+
+       assert(service.last_key_update() > service2.last_key_update())
+       assert(service.last_certificate().public_key() ==
+              service2.public_certificate())
+       assert(service.last_key() == service2.private_key())
+    except:
+       pop_is_running_service()
+       pop_testing_objstore()
+       raise
+
+    pop_is_running_service()
+    pop_testing_objstore()
