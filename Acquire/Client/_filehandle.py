656c8a795178c357f3e099b56b4825dcc92b050f
Christopher Woods <chryswoods@gmail.com>
2019-03-12 09:11:31 +0000

    Made version into a separate objectstore object, and used non-clashing naming so that I don't need to use a mutex to protect simultaneous uploads of the same file. This should always work, even with high contention :-)

--- Acquire/Client/_filehandle.py
+++ Acquire/Client/_filehandle.py
@@ -24,10 +24,6 @@ class FileHandle:
 
             self._filesize = filesize
             self._checksum = cksum
-
-            from Acquire.Storage import ACLRule as _ACLRule
-            self._acl = _ACLRule.owner()
-            self._version = None
         else:
             self._filename = None
 
@@ -53,35 +49,9 @@ class FileHandle:
         else:
             return self._checksum
 
-    def acl(self):
-        """Return the ACL rule for the current user for this file"""
-        return self._acl
-
-    def version(self):
-        """Return the version of this file on the storage service. This
-           is a datetime of the upload of this version. You will need to
-           query the storage service to find if there are other versions.
-           This returns None if this file has not yet been uploaded
-        """
-        if self.is_null():
-            return None
-        else:
-            return self._version
-
     def fingerprint(self):
         """Return a fingerprint for this file"""
         return "%s-%s-%s" % (self.filename(), self.filesize(), self.checksum())
-
-    def add_old_versions(self, old_fileinfo):
-        """Record an old version of this file described in the passed
-           'old_fileinfo'
-        """
-        if self.is_null() or old_fileinfo.is_null():
-            return
-
-        # somehow match filenames and acls... (should inherit ACLS?)
-
-        # add the versions to our dictionary
 
     def to_data(self):
         """Return a json-serialisable dictionary for this object"""
@@ -93,8 +63,6 @@ class FileHandle:
             data["filename"] = self.filename()
             data["filesize"] = self.filesize()
             data["checksum"] = self.checksum()
-            data["acl"] = self.acl().to_data()
-            data["version"] = _datetime_to_string(self.version())
 
         return data
 
@@ -106,14 +74,8 @@ class FileHandle:
         f = FileHandle()
 
         if data is not None and len(data) > 0:
-            from Acquire.Storage import ACLRule as _ACLRule
-            from Acquire.ObjectStore import string_to_datetime \
-                as _string_to_datetime
-
             f._filename = data["filename"]
             f._filesize = int(data["filesize"])
             f._checksum = data["checksum"]
-            f._acl = _ACLRule.from_data(data["acl"])
-            f._version = _string_to_datetime(data["version"])
 
         return f
--- Acquire/Storage/_fileinfo.py
+++ Acquire/Storage/_fileinfo.py
@@ -1,18 +1,46 @@
 
 __all__ = ["FileInfo", "VersionInfo"]
 
+_version_root = "storage/versions"
+_fileinfo_root = "storage/files"
+
 
 class VersionInfo:
     """This class holds specific info about a version of a file"""
-    def __init__(self, filesize=None, checksum=None):
+    def __init__(self, filesize=None, checksum=None,
+                 acls=None, user_guid=None):
         """Construct the version of the file that has the passed
-           size and checksum
+           size and checksum, was uploaded by the specified user,
+           and that has the specified acls
         """
         if filesize is not None:
             from Acquire.ObjectStore import create_uuid as _create_uuid
+            from Acquire.ObjectStore import get_datetime_now \
+                as _get_datetime_now
+            from Acquire.ObjectStore import datetime_to_string \
+                as _datetime_to_string
+            from Acquire.Storage import ACLRule as _ACLRule
+
             self._filesize = filesize
             self._checksum = checksum
             self._file_uid = _create_uuid()
+            self._user_guid = str(user_guid)
+
+            self._acls = {}
+
+            if acls is not None:
+                for user, acl in acls.items():
+                    if not isinstance(acl, _ACLRule):
+                        raise TypeError(
+                            "ACL must be of type ACLRule")
+
+                    if user is not None:
+                        self._acls[str(user)] = acl
+                    else:
+                        self._acls[user] = acl
+
+            self._datetime = _get_datetime_now()
+
         else:
             self._filesize = None
 
@@ -34,6 +62,17 @@ class VersionInfo:
         else:
             return self._checksum
 
+    def acl(self, user_guid):
+        """Return the ACL for this version of the file for the user
+           with passed GUID. This ACL will override any ACL inherited
+           from the drive
+        """
+        try:
+            return self._acls[user_guid]
+        except:
+            from Acquire.Storage import ACLRule as _ACLRule
+            return _ACLRule.inherit()
+
     def uid(self):
         """Return the UID of this version of the file in object store"""
         if self.is_null():
@@ -41,14 +80,46 @@ class VersionInfo:
         else:
             return self._file_uid
 
+    def datetime(self):
+        """Return the datetime when this version was created"""
+        if self.is_null():
+            return self._datetime
+        else:
+            return None
+
+    def uploaded_by(self):
+        """Return the GUID of the user that uploaded this version"""
+        if self.is_null():
+            return self._user_guid
+        else:
+            return None
+
+    def _key(self, encoded_filename):
+        """Return the key for this version in the object store"""
+        if self.is_null():
+            return None
+        else:
+            from Acquire.ObjectStore import datetime_to_string \
+                as _datetime_to_string
+            return "%s/%s/%s/%s" % (
+                _version_root, encoded_filename,
+                _datetime_to_string(self._datetime), self._file_uid)
+
     def to_data(self):
         """Return a json-serialisable dictionary for this object"""
         data = {}
 
         if not self.is_null():
-            data["filesize"] = self.filesize()
-            data["checksum"] = self.checksum()
-            data["uid"] = self.uid()
+            from Acquire.ObjectStore import datetime_to_string \
+                as _datetime_to_string
+            from Acquire.ObjectStore import dict_to_string \
+                as _dict_to_string
+            data["filesize"] = self._filesize
+            data["checksum"] = self._checksum
+            data["uid"] = self._uid
+            data["datetime"] = _datetime_to_string(self._datetime)
+            data["user_guid"] = self._user_guid
+            data["acls"] = _dict_to_string(self._acls)
 
         return data
 
@@ -60,9 +131,17 @@ class VersionInfo:
         v = VersionInfo()
 
         if data is not None and len(data) > 0:
+            from Acquire.ObjectStore import string_to_datetime \
+                as _string_to_datetime
+            from Acquire.ObjectStore import string_to_dict \
+                as _string_to_dict
+            from Acquire.Storage import ACLRule as _ACLRule
             v._filesize = data["filesize"]
             v._checksum = data["checksum"]
             v._uid = data["uid"]
+            v._user_guid = data["user_guid"]
+            v._datetime = _string_to_datetime(data["datetime"])
+            v._acls = _string_to_dict(data["acls"], _ACLRule)
 
         return v
 
@@ -93,16 +172,21 @@ class FileInfo:
             if filehandle.is_null():
                 return
 
-            from Acquire.ObjectStore import get_datetime_now \
-                as _get_datetime_now
+            from Acquire.ObjectStore import string_to_encoded \
+                as _string_to_encoded
 
             self._filename = filehandle.filename()
+            self._encoded_filename = _string_to_encoded(self._filename)
 
             version = VersionInfo(filesize=filehandle.filesize(),
-                                  checksum=filehandle.checksum())
+                                  checksum=filehandle.checksum(),
+                                  user_guid=user_guid,
+                                  acls=None)
 
-            self._versions = {_get_datetime_now(): version}
-            self._acls = {user_guid: filehandle.acl()}
+            self._latest_version = version
+
+            # save this to the object store
+            self.save()
 
     def is_null(self):
         """Return whether or not this is null"""
@@ -175,6 +259,32 @@ class FileInfo:
         v = list(self._versions.keys())
         v.sort()
         return v
+
+    def _key(self):
+        """Return the key for this fileinfo in the object store"""
+        return "%s/%s" % (_fileinfo_root, self._encoded_filename)
+
+    def save(self):
+        """Save this fileinfo to the object store"""
+        if self.is_null():
+            return
+
+        from Acquire.ObjectStore import ObjectStore as _ObjectStore
+        from Acquire.Service import get_service_account_bucket \
+            as _get_service_account_bucket
+
+        bucket = _get_service_account_bucket()
+
+        # save the fileinfo itself
+        _ObjectStore.set_object_from_json(bucket=bucket,
+                                          key=self._key(),
+                                          data=self.to_data())
+
+        # also save the version information (saves old versions)
+        _ObjectStore.set_object_from_json(
+                        bucket=bucket,
+                        key=self._latest_version._key(self._encoded_filename),
+                        data=self._latest_version.to_data())
 
     def to_data(self):
         """Return a json-serialisable dictionary for this object"""
