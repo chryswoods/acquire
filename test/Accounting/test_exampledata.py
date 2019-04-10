86096f807836a685093875b16859a6d268ff6cb0
Christopher Woods <chryswoods@gmail.com>
2019-04-10 11:48:13 +0100

    I've added example data copied from the live service that has allowed me to reproduce the accounting bug. I will now fix this locally before pushing a fix to the service.
    I've also made a quick fix to the freezetime test so that it can be run
    standalone
    
    ***NO_CI***

---
+++ test/Accounting/test_exampledata.py
@@ -0,0 +1,44 @@
+
+import pytest
+import os
+import distutils
+from distutils import dir_util
+
+from Acquire.Accounting import Account, Accounts
+
+from Acquire.Service import push_is_running_service, get_service_account_bucket
+
+
+@pytest.fixture(scope="session")
+def bucket(tmpdir_factory):
+    try:
+        return get_service_account_bucket()
+    except:
+        d = tmpdir_factory.mktemp("objstore")
+        push_is_running_service()
+        return get_service_account_bucket(str(d))
+
+
+def test_example_data(bucket):
+    example_dir = "%s/example_data" % os.path.split(__file__)[0]
+
+    assert(os.path.exists("%s/accounting" % example_dir))
+
+    # copy the accounting data into the object store
+    distutils.dir_util.copy_tree(example_dir, bucket)
+
+    user1_accounts = Accounts("e2e31e35-025c-4a4c-8a7b-65da94e722d6")
+    user2_accounts = Accounts("e5e03b59-bc04-4a7f-9ebd-9c32dbc795f6")
+
+    accounts = []
+
+    for account in user1_accounts.list_accounts():
+        accounts.append(user1_accounts.get_account(account))
+
+    for account in user2_accounts.list_accounts():
+        accounts.append(user2_accounts.get_account(account))
+
+    for account in accounts:
+        print(account.name(), account.balance())
+
+    assert(False)
