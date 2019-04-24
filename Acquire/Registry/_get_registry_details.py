7a463c06131dca675e91b5afad0f309e80b25861
Christopher Woods <chryswoods@gmail.com>
2019-04-24 18:36:07 +0100

    Added public key and cert for the default primary registry for Acquire

--- Acquire/Registry/_get_registry_details.py
+++ Acquire/Registry/_get_registry_details.py
@@ -10,8 +10,35 @@ _testing_registry = {
 _acquire_registry = {
     "canonical_url": "http://fn.acquire-aaai.com:8080/t/registry",
     "uid": "a0-a0",
-    "public_key": None,
-    "public_certificate": None}
+    "public_key": {"bytes": "LS0tLS1CRUdJTiBQVUJMSUMgS0VZLS0tLS0KTUlJQklqQU"
+                            "5CZ2txaGtpRzl3MEJBUUVGQUFPQ0FROEFNSUlCQ2dLQ0FR"
+                            "RUF1SE1iMzNUYytTZjFkVFBMMk9ESApqSWcxQjJYMENBeV"
+                            "hVMVl4ejI5aWl5cTJlQm5FZGFuQXNTZ2oxS0dYbUJCRHdr"
+                            "N2Y4SmJNWDVNOE5hbm9jVXRWClZ2aW1jbm1LZVhRRVQweTh"
+                            "VM216NzR2OUZid056azYxdkpNRXhHa0tyVzRjdi81b1dlYU"
+                            "FEdmVLSkJ5UzEwYnUKSjlYVU9IZTZIR1B2bWYyelk2QVBkN"
+                            "2JCTHFlN3lMVjAvSi93WS9sOGlYNHNtc1gxM2NsY0s4YXpj"
+                            "elIzN1pYQQpSQkhkKzlOSllnS2ZqbXB4akxYcWpIN3R4cUh"
+                            "RTzNYS3Z3blhiOXlUbVV4TEg2aEdLQ2xiZ2l6RGdCNjRQWD"
+                            "dnCk5xb2c2STBFVWl5bEhlNjZ6aXFRcm5QZWVtUjNzNm03T"
+                            "2RwS2Y2eU8vd2dmaXpVaVBhSkwvS0xaTHplQXc2RFkKcHdJ"
+                            "REFRQUIKLS0tLS1FTkQgUFVCTElDIEtFWS0tLS0tCg=="},
+    "public_certificate": {"bytes": "LS0tLS1CRUdJTiBQVUJMSUMgS0VZLS0tLS0KT"
+                                    "UlJQklqQU5CZ2txaGtpRzl3MEJBUUVGQUFPQ0"
+                                    "FROEFNSUlCQ2dLQ0FRRUF4RWRvYTlLRUkvS0N"
+                                    "zN1UxdGlLTwpwbUN5UVRnU2hBb1lXTW9jTHJWN"
+                                    "GFQRStGdVdQYzBlWlRSeEdqSDBJOEN4bXZub2p"
+                                    "IYVd2dDN3Tm9Jb2s5b2JICkcwazI3blFnZ05jO"
+                                    "EYvWUpKZ1JVcVRkdExDdkFWOEcxRWN5bDQrS1l"
+                                    "1UEIzNFpIMTFqUit1V3RqUzAvc2JJbnUKV3JiV"
+                                    "WhObFVwemJxbm8yT2UvVXIvSHJOZ2liUXM1eFR"
+                                    "Od3BvK2NWMkpiM0NDM21YUkxUWkZpaFMwY1lXN"
+                                    "ytLSwoyQ1I5QUM4UnltZ1h3TUUzTXUvT2JYV1F"
+                                    "UMk0vSzNlcUJxYkZjUFZLL04zOFhERS8xbnlvR"
+                                    "1djTnBsWWNHU3oyCjNrZWhsWUNTVWh1T01tbGt"
+                                    "3NkFQMllDR05oRzNXRjdYZUlTdXZvMWovMnlNN"
+                                    "2pIY2gzZjZjcWRvWWNLNk5Lc0IKQXdJREFRQUI"
+                                    "KLS0tLS1FTkQgUFVCTElDIEtFWS0tLS0tCg=="}}
 
 _registries = {_testing_registry["uid"]: _testing_registry,
                _acquire_registry["uid"]: _acquire_registry}
