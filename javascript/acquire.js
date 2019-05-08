74cddfeb38cf7883f1afc3faabc32ac8b13f2c6d
Christopher Woods <chryswoods@gmail.com>
2019-05-08 18:23:06 +0100

    Can now save and load public keys and services

--- javascript/acquire.js
+++ javascript/acquire.js
@@ -38,6 +38,25 @@ function string_to_datetime(s)
     return datetime_to_datetime(Date.parse(s));
 }
 
+/** Convert the passed string back to bytes */
+function string_to_bytes(s)
+{
+    s = unescape(encodeURIComponent(s));
+    return atob(s);
+}
+
+/** Convert the passed string to a utf-8 string */
+function string_to_utf8_bytes(s)
+{
+    return new TextEncoder("utf-8").encode(s);
+}
+
+/** Convert the passed bytes to a safe string */
+function bytes_to_string(b)
+{
+    return btoa(b);
+}
+
 /** Call the specified URL */
 async function call_function({service_url=undefined, func=undefined,
                              args=undefined, args_key=undefined,
@@ -101,22 +120,142 @@ async function call_function({service_url=undefined, func=undefined,
     return payload.return;
 }
 
+/** Function used as part of converting a key to a pem file */
+function _arrayBufferToBase64String(arrayBuffer)
+{
+    var byteArray = new Uint8Array(arrayBuffer)
+    var byteString = ''
+    for (var i=0; i<byteArray.byteLength; i++) {
+        byteString += String.fromCharCode(byteArray[i])
+    }
+    return btoa(byteString)
+}
+
+/** Function to convert a base64 string to an array buffer */
+function _base64StringToArrayBuffer(b64str)
+{
+    var byteStr = atob(b64str)
+    var bytes = new Uint8Array(byteStr.length)
+    for (var i = 0; i < byteStr.length; i++) {
+        bytes[i] = byteStr.charCodeAt(i)
+    }
+    return bytes.buffer
+}
+
+/** Function used to convert binary key date to pem */
+function _convertBinaryToPem(binaryData, label) {
+    var base64Cert = _arrayBufferToBase64String(binaryData)
+    var pemCert = "-----BEGIN " + label + "-----\n"
+    var nextIndex = 0
+    var lineLength
+    while (nextIndex < base64Cert.length) {
+        if (nextIndex + 64 <= base64Cert.length) {
+        pemCert += base64Cert.substr(nextIndex, 64) + "\n"
+        } else {
+        pemCert += base64Cert.substr(nextIndex) + "\n"
+        }
+        nextIndex += 64
+    }
+    pemCert += "-----END " + label + "-----\n"
+    return pemCert
+}
+
+/** Function to convert pemfile info binary data used for js crypto */
+function _convertPemToBinary(pem)
+{
+    var lines = pem.split('\n')
+    var encoded = ''
+    for(var i = 0;i < lines.length;i++){
+        if (lines[i].trim().length > 0 &&
+            lines[i].indexOf('-BEGIN PRIVATE KEY-') < 0 &&
+            lines[i].indexOf('-BEGIN PUBLIC KEY-') < 0 &&
+            lines[i].indexOf('-END PRIVATE KEY-') < 0 &&
+            lines[i].indexOf('-END PUBLIC KEY-') < 0) {
+        encoded += lines[i].trim()
+        }
+    }
+    return _base64StringToArrayBuffer(encoded)
+}
+
+/** Hard code the key size (in bytes) as javascript web crypto doesn't
+ *  seem to have a way to query this programatically. 256 bytes (2048 bit)
+ *  is used on the server in all of the python functions
+*/
+var _rsa_key_size = 256;
+
+/** Function to import and return the public key from the passed pemfile */
+async function _importPublicKey(pemKey)
+{
+    //convert the pem key to binary
+    var bin = _convertPemToBinary(pemKey);
+
+    var encryptAlgorithm = {
+        name: "RSA-OAEP",
+        modulusLength: 8*_rsa_key_size,
+        publicExponent: 65537,
+        extractable: false,
+        hash: {
+            name: "SHA-256"
+        }
+    };
+
+    var public_key = await crypto.subtle.importKey(
+                        "spki", bin, encryptAlgorithm,
+                        true, ["encrypt"]
+                        );
+
+    return public_key;
+}
+
+/** Function to convert a public key to a PEM file */
+async function _exportPublicKey(key) {
+    let exported = await window.crypto.subtle.exportKey('spki', key);
+    let pem = _convertBinaryToPem(exported, "PUBLIC KEY");
+    return pem;
+}
+
+/** This class provides a simple handle to a public key. This
+ *  can be used to encrypt data for Acquire, and also to
+ *  verify signatures
+ */
 class PublicKey
 {
     constructor()
     {
-
+        this._key = undefined;
     }
 
-    to_data()
+    is_null()
     {
-        data = {};
+        return this._key == undefined;
+    }
+
+    async to_data()
+    {
+        var data = {};
+
+        if (this.is_null()){ return data; }
+
+        var pem = await _exportPublicKey(this._key);
+        var b = bytes_to_string(pem);
+
+        data["bytes"] = b;
+
         return data;
     }
 
-    static from_data(data)
+    static async from_data(data)
     {
+        if (data == undefined){ return undefined;}
+
         var key = new PublicKey();
+
+        var b = data["bytes"];
+
+        var pem = string_to_bytes(data["bytes"]);
+
+        key._key = await _importPublicKey(pem);
+
         return key;
     }
 }
@@ -170,7 +309,7 @@ class Service
         }
     }
 
-    to_data()
+    async to_data()
     {
         var data = {};
 
@@ -187,23 +326,8 @@ class Service
         data["service_user_uid"] = this._service_user_uid;
         data["service_user_name"] = this._service_user_name;
 
-        try
-        {
-            data["public_key"] = this._pubkey.to_data();
-        }
-        catch
-        {
-            data["public_key"] = undefined;
-        }
-
-        try
-        {
-            data["public_certificate"] = this._pubcert.to_data();
-        }
-        catch
-        {
-            data["public_certificate"] = undefined;
-        }
+        data["public_key"] = await this._pubkey.to_data();
+        data["public_certificate"] = await this._pubcert.to_data();
 
         data["last_key_update"] = datetime_to_string(this._last_key_update);
         data["key_update_interval"] = this._key_update_interval;
@@ -211,7 +335,7 @@ class Service
         return data;
     }
 
-    static from_data(data)
+    static async from_data(data)
     {
         var service = new Service();
 
@@ -226,8 +350,9 @@ class Service
         service._service_user_uid = data["service_user_uid"];
         service._service_user_name = data["service_user_name"];
 
-        service._pubkey = PublicKey.from_data(data["public_key"])
-        service._pubcert = PublicKey.from_data(data["public_certificate"])
+        service._pubkey = await PublicKey.from_data(data["public_key"])
+        service._pubcert = await PublicKey.from_data(
+                                            data["public_certificate"])
 
         service._last_key_update = string_to_datetime(data["last_key_update"])
         service._key_update_interval = parseFloat(data["key_update_interval"])
@@ -246,11 +371,11 @@ async function test_acquire()
 
     console.log(response);
 
-    var s = Service.from_data(response["service_info"]);
+    var s = await Service.from_data(response["service_info"]);
     console.log(s);
-    data = s.to_data();
+    var data = await s.to_data();
     console.log(data);
     console.log(s.registry_uid());
-    var s2 = Service.from_data(data);
+    var s2 = await Service.from_data(data);
     console.log(s2);
 }
