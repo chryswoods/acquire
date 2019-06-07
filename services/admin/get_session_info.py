d01c7b97498872c0000121c9bb39e1f7af546e2f
Christopher Woods <chryswoods@gmail.com>
2019-06-07 16:49:14 +0100

    Working on getting the login message so that the user can verify the login session is correct

--- services/admin/get_session_info.py
+++ services/admin/get_session_info.py
@@ -8,7 +8,15 @@ def run(args):
     """This function will allow anyone to obtain the public
        keys for the passed login session
     """
-    session_uid = args["session_uid"]
+    try:
+        session_uid = args["session_uid"]
+    except:
+        session_uid = None
+
+    try:
+        short_uid = args["short_uid"]
+    except:
+        short_uid = None
 
     try:
         scope = args["scope"]
@@ -20,8 +28,17 @@ def run(args):
     except:
         permissions = None
 
-    login_session = LoginSession.load(uid=session_uid, scope=scope,
-                                      permissions=permissions)
+    if session_uid:
+        login_session = LoginSession.load(uid=session_uid, scope=scope,
+                                          permissions=permissions)
+    else:
+        if short_uid is None:
+            raise PermissionError(
+                "You must specify either the session_uid or the short_uid "
+                "of the login session")
+
+        login_session = LoginSession.load(short_uid=short_uid, scope=scope,
+                                          permissions=permissions)
 
     return_value = {}
 
