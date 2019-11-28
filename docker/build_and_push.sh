14d5240a68e1a9860b54e83a4ffc24f3f08e9a6d
Gareth Jones <oxidising@gmail.com>
2019-11-28 21:58:17 +0000

    WIP: Testing newer fdk version for fix of error code handling

--- docker/build_and_push.sh
+++ docker/build_and_push.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-rsync -a --verbose ../../Acquire . --exclude '__pycache__'
+rsync -a --verbose ../../Acquire . --exclude '__pycache__' --exclude '.git'
 rsync -a --verbose ../admin . --exclude '__pycache__'
 
-docker build -t chryswoods/acquire-base:latest .
+docker build -t garj/acquire-base:latest .
 
 rm -rf Acquire admin
 
-#docker push chryswoods/acquire-base:latest
+docker push garj/acquire-base:latest
 
 #docker run --rm -it chryswoods/acquire-base:latest
