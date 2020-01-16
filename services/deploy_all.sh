f5ceddb5b553bbbf4e92234df444095f247b29b9
Christopher Woods <chryswoods@gmail.com>
2020-01-16 14:02:03 +0000

    Changes operational fn server

--- services/deploy_all.sh
+++ services/deploy_all.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-cd base_image && ./build_and_push.sh && cd -
-cd identity && fn --verbose deploy --local --all && cd -
-cd access && fn --verbose deploy --local --all && cd -
-cd accounting && fn --verbose deploy --local --all && cd -
-cd storage && fn --verbose deploy --local --all && cd -
-cd compute && fn --verbose deploy --local --all && cd -
-#cd registry && fn --verbose deploy --local --all & cd -
+cd base_image && ./build_and_push.sh && cd ..
+cd access && fn --verbose deploy --local --all && cd ..
+cd accounting && fn --verbose deploy --local --all && cd ..
+cd storage && fn --verbose deploy --local --all && cd ..
+cd compute && fn --verbose deploy --local --all && cd ..
+cd identity && fn --verbose deploy --local --all && cd .. 
